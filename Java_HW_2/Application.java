c81bb38b3f659041d30ddc0f00adc6c45f772890
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2023-02-17 10:54:35 +0300

    основной модуль

---
+++ Java_HW_2/Application.java
@@ -0,0 +1,6 @@
+package Java_HW_2;
+public class Application {
+    public static void main(String[] args) throws Exception{
+        OutputWriter.print(ReadInput.inputReader("a"),ReadInput.inputReader("b"));
+    }
+}
