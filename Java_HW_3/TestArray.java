37410e5a31d392a820b804c558c7496693412746
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2023-02-20 00:13:15 +0300

    Тестовый массив

--- Java_HW_3/TestArray.java
+++ Java_HW_3/TestArray.java
@@ -1,2 +1,12 @@
-package Java_HW_3;public class TestArray {
+package Java_HW_3;
+
+import java.util.Arrays;
+
+public class TestArray {
+    public static void main(String[] args) {
+        int[] a = {74, 58, 54, 48, 29, 15, 65, 88, 20};
+        int[] b = MergeSort.sortMerge(a);
+        System.out.println(Arrays.toString(a));
+        System.out.println(Arrays.toString(b));
+    }
 }
