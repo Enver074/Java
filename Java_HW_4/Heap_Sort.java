2792b0a87cb38185af63e5b7232d27117ec69c92
Enver Ismailov <113299648+Enver074@users.noreply.github.com>
2023-02-26 10:25:37 +0300

    Пирамидальная сортировка

--- Java_HW_4/Heap_Sort.java
+++ Java_HW_4/Heap_Sort.java
@@ -1,5 +1,5 @@
 public class Heap_Sort {
-    public static void main(String args[]){
+    public static void main(String args[]){  // Управляющая программа
         int[] arr = {2121, 48, 7, 565, 77, 69, 56, 4};
 
         heapSort(arr); 
@@ -7,21 +7,27 @@ public class Heap_Sort {
         
     }
 
-    private static void heapSort(int[] arr) {
+    private static void heapSort(int[] arr) {   // Реализация пирамидальной сортировки на Java
         int n = arr.length;
+
+        // Построение кучи (перегруппируем массив)
         for(int i = n / 2 - 1; i >= 0; i--)
             heapify(arr, i, n);
-
+        
+        // Один за другим извлекаем элементы из кучи 
         for (int i = n - 1; i >= 0; i--){
             int temp = arr[i];
             arr[i] = arr[0];
             arr[0] = temp;
-
+            
+            // Вызываем процедуру heapify на уменьшенной куче
             heapify(arr, 0, i);
         }
     }
     
-    private static void heapify(int[] arr, int i, int n) {
+    // Процедура для преобразования в двоичную кучу поддерева с корневым узлом i, что является
+    // индексом в arr[]. n - размер кучи
+    private static void heapify(int[] arr, int i, int n) {  
         int l = i * 2 + 1;
         int r = i * 2 + 2;
         int largest = i;
@@ -39,7 +45,7 @@ public class Heap_Sort {
         }
     }
 
-    static void printArray(int arr[])
+    static void printArray(int arr[])  // функция вывода массива
     {
         int n = arr.length;
         for (int i=0; i<n; ++i)
